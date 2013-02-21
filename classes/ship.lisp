037e703992190a63175be2732f2b00e4ae1a8dc0
Yuri Albuquerque <yuri.albuquerque@tap4mobile.com.br>
2013-02-21 18:15:52 -0400

    Drawing a ship on the screen.

--- classes/ship.lisp
+++ classes/ship.lisp
@@ -3,18 +3,32 @@
 (in-package #:space-invaders)
 
 (defclass ship ()
-    ((pos
-      :initarg :pos
-      :accessor pos
-      :initform #(0 0))
-     (image
-      :initarg :image
-      :reader image
-      :initform nil)
-     (image-with-zoom
-      :reader image-with-zoom
-      :initform nil)
-     (zoom
-      :initarg :zoom
-      :reader zoom
-      :initform #(1 1))))
+  ((pos
+    :initarg :pos
+    :accessor pos
+    :initform #(0 0))
+   (image
+    :initarg :image
+    :reader image
+    :initform nil)
+   (image-with-zoom
+    :reader image-with-zoom
+    :initform nil)
+   (zoom
+    :initarg :zoom
+    :reader zoom
+    :initform #(1 1))))
+
+(defmethod initialize-instance :after ((this ship) &key)
+  (with-slots (zoom image-with-zoom image) this
+    (when image
+      (setf image-with-zoom (sdl:zoom-surface (elt zoom 0) (elt zoom 1) :surface image)))))
+
+(defgeneric draw (this)
+  (:documentation "Draws the ship on the screen"))
+
+(defmethod draw ((this ship))
+  "Draws the ship on the screen"
+  (with-accessors ((image image-with-zoom) (pos pos)) this
+    (when image
+      (sdl:draw-surface-at-* image (elt pos 0) (elt pos 1)))))
--- space-invaders.lisp
+++ space-invaders.lisp
@@ -1,3 +1,15 @@
 ;;;; space-invaders.lisp
 
 (in-package #:space-invaders)
+
+(defun main ()
+  (setf *default-pathname-defaults* (asdf:system-source-directory 'space-invaders))
+  (sdl:with-init ()
+    (sdl:window 800 600)
+    (let ((ship1 (make-instance 'ship :image (sdl:load-image
+                                              (merge-pathnames #p"assets/nave1.png")))))
+      (draw ship1)
+      (sdl:update-display)
+      (sdl:with-events ()
+        (:quit-event () t)
+        (:video-expose-event () (sdl:update-display))))))
