b770bb938a6cf65e490c234c09460679586c8f94
Yuri Albuquerque <yuridenommus@gmail.com>
2013-03-19 13:03:50 -0400

    Following the code convention for predicates.

--- classes/bullet.lisp
+++ classes/bullet.lisp
@@ -11,14 +11,14 @@
    (zoom
     :initarg :zoom
     :reader zoom
-    :initform #(1/10 1/10))))
+    :initform #(1/5 1/5))))
 
 (defmethod update ((this bullet))
   "Updates the bullet"
   (with-accessors ((pos pos) (scene-manager scene-manager)) this
     (setf pos (map 'vector #'+ pos #(0 -10)))
     (map nil (lambda (node)
-               (when (collide this node)
+               (when (collide-p this node)
                  (setf (scene-objects scene-manager)
                        (delete node (scene-objects scene-manager)))
                  (setf (scene-objects scene-manager)
@@ -27,13 +27,13 @@
     (when (< (elt pos 1) 0)
       (setf (scene-objects scene-manager) (delete this (scene-objects scene-manager))))))
 
-(defgeneric collide (bullet enemy)
+(defgeneric collide-p (bullet enemy)
   (:documentation "Tests collision between the bullet and an enemy"))
 
-(defmethod collide ((bullet bullet) enemy)
+(defmethod collide-p ((bullet bullet) enemy)
   nil)
 
-(defmethod collide ((bullet bullet) (enemy enemy))
+(defmethod collide-p ((bullet bullet) (enemy enemy))
   (< (reduce #'+ (map 'vector
                       (lambda (x y) (expt (- x y) 2))
                       (pos bullet)
